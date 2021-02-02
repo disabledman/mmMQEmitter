0db5da7be0b1b489bc694b50bf6e374956e9258c
謝德建 <disabledman@hotmail.com>
2021-02-02 15:43:49 +0800

    加入專案檔案。

---
+++ mmMQEmitter/Form1.Designer.cs
@@ -0,0 +1,396 @@
+﻿
+namespace mmMQEmitter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxServer = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxAccount = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.textBoxRouteKey = new System.Windows.Forms.TextBox();
+            this.textBoxOutQuene = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxExchange = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBoxInQuene = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.textBoxInterval = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxBody = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.listBoxLog = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBoxPassword);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBoxServer);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBoxAccount);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(14, 14);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox1.Size = new System.Drawing.Size(361, 179);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "MQ Info";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(96, 123);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(240, 29);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 126);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(80, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password";
+            // 
+            // textBoxServer
+            // 
+            this.textBoxServer.Location = new System.Drawing.Point(96, 30);
+            this.textBoxServer.Name = "textBoxServer";
+            this.textBoxServer.Size = new System.Drawing.Size(240, 29);
+            this.textBoxServer.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 33);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Server";
+            // 
+            // textBoxAccount
+            // 
+            this.textBoxAccount.Location = new System.Drawing.Point(96, 88);
+            this.textBoxAccount.Name = "textBoxAccount";
+            this.textBoxAccount.Size = new System.Drawing.Size(240, 29);
+            this.textBoxAccount.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 91);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Account";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.buttonDisconnect);
+            this.groupBox2.Controls.Add(this.buttonConnect);
+            this.groupBox2.Controls.Add(this.textBoxRouteKey);
+            this.groupBox2.Controls.Add(this.textBoxOutQuene);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.textBoxExchange);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.textBoxInQuene);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(385, 14);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox2.Size = new System.Drawing.Size(728, 179);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Quene Config";
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(570, 113);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(114, 47);
+            this.buttonDisconnect.TabIndex = 5;
+            this.buttonDisconnect.Text = "DisConnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(433, 113);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(114, 47);
+            this.buttonConnect.TabIndex = 5;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // textBoxRouteKey
+            // 
+            this.textBoxRouteKey.Location = new System.Drawing.Point(444, 65);
+            this.textBoxRouteKey.Name = "textBoxRouteKey";
+            this.textBoxRouteKey.Size = new System.Drawing.Size(240, 29);
+            this.textBoxRouteKey.TabIndex = 1;
+            // 
+            // textBoxOutQuene
+            // 
+            this.textBoxOutQuene.Location = new System.Drawing.Point(96, 65);
+            this.textBoxOutQuene.Name = "textBoxOutQuene";
+            this.textBoxOutQuene.Size = new System.Drawing.Size(240, 29);
+            this.textBoxOutQuene.TabIndex = 1;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(363, 68);
+            this.label10.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(81, 20);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "RouteKey";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 68);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "OUT";
+            // 
+            // textBoxExchange
+            // 
+            this.textBoxExchange.Location = new System.Drawing.Point(444, 30);
+            this.textBoxExchange.Name = "textBoxExchange";
+            this.textBoxExchange.Size = new System.Drawing.Size(240, 29);
+            this.textBoxExchange.TabIndex = 1;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(363, 33);
+            this.label9.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(82, 20);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Exchange";
+            // 
+            // textBoxInQuene
+            // 
+            this.textBoxInQuene.Location = new System.Drawing.Point(96, 30);
+            this.textBoxInQuene.Name = "textBoxInQuene";
+            this.textBoxInQuene.Size = new System.Drawing.Size(240, 29);
+            this.textBoxInQuene.TabIndex = 1;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 33);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "IN";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.buttonStop);
+            this.groupBox3.Controls.Add(this.buttonStart);
+            this.groupBox3.Controls.Add(this.textBoxInterval);
+            this.groupBox3.Controls.Add(this.label8);
+            this.groupBox3.Controls.Add(this.label6);
+            this.groupBox3.Controls.Add(this.textBoxBody);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Location = new System.Drawing.Point(14, 203);
+            this.groupBox3.Margin = new System.Windows.Forms.Padding(5);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Padding = new System.Windows.Forms.Padding(5);
+            this.groupBox3.Size = new System.Drawing.Size(1099, 210);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Send";
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(941, 155);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(114, 47);
+            this.buttonStop.TabIndex = 5;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.buttonStop_Click);
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(804, 155);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(114, 47);
+            this.buttonStart.TabIndex = 4;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // textBoxInterval
+            // 
+            this.textBoxInterval.Location = new System.Drawing.Point(96, 123);
+            this.textBoxInterval.Name = "textBoxInterval";
+            this.textBoxInterval.Size = new System.Drawing.Size(121, 29);
+            this.textBoxInterval.TabIndex = 3;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(225, 126);
+            this.label8.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(153, 20);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "ms (0 : Single Time)";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 126);
+            this.label6.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 20);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Interval";
+            // 
+            // textBoxBody
+            // 
+            this.textBoxBody.Location = new System.Drawing.Point(96, 30);
+            this.textBoxBody.Multiline = true;
+            this.textBoxBody.Name = "textBoxBody";
+            this.textBoxBody.Size = new System.Drawing.Size(995, 87);
+            this.textBoxBody.TabIndex = 1;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(15, 33);
+            this.label7.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 20);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Body";
+            // 
+            // listBoxLog
+            // 
+            this.listBoxLog.FormattingEnabled = true;
+            this.listBoxLog.ItemHeight = 20;
+            this.listBoxLog.Location = new System.Drawing.Point(12, 421);
+            this.listBoxLog.Name = "listBoxLog";
+            this.listBoxLog.Size = new System.Drawing.Size(1105, 384);
+            this.listBoxLog.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1127, 825);
+            this.Controls.Add(this.listBoxLog);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "MQ Emitter";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxServer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxAccount;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox textBoxInQuene;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxOutQuene;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.TextBox textBoxBody;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ListBox listBoxLog;
+        private System.Windows.Forms.TextBox textBoxInterval;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.TextBox textBoxRouteKey;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxExchange;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ mmMQEmitter/Form1.cs
@@ -0,0 +1,208 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mmMQEmitter
+{
+    public partial class Form1 : Form
+    {
+        private ConnectionFactory m_Factory = null;
+        private IConnection m_Connection = null;
+        private IModel m_InChannel = null;
+        private IModel m_OutChannel = null;
+        private EventingBasicConsumer m_InConsumer = null;
+        private bool m_bIsMQConnect = false;
+
+        //
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBoxInterval.Text = "0";
+            textBoxOutQuene.Text = "MQ.SocketAgent";
+            textBoxInQuene.Text = "MQ.SocketEap";
+            textBoxAccount.Text = "guest";
+            textBoxPassword.Text = "guest";
+            textBoxServer.Text = "10.1.1.23";// "127.0.0.1";
+            textBoxExchange.Text = "topic";
+            textBoxRouteKey.Text = "#";
+
+            textBoxBody.Text = "HELLO";
+
+            listBoxLog.Items.Clear();
+        }
+
+        //
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            buttonDisconnect_Click(this, null);
+        }
+
+        //
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (m_bIsMQConnect == false) return;
+
+            if(!int.TryParse(textBoxInterval.Text, out int interval))
+            {
+                _Log($"Input interval not a NUMBER. [{textBoxInterval.Text}]");
+            }
+
+            _Log($"Send interval is {interval} ms.");
+
+            if (interval == 0)
+            {
+                _Log("Send Body one time only.");
+                return;
+            }
+
+            //
+            timer1.Interval = interval;
+            timer1.Start();
+        }
+
+        //
+        private void _SendBody()
+        {
+            if (m_bIsMQConnect == false) return;
+
+            var body = Encoding.UTF8.GetBytes(textBoxBody.Text);
+
+            m_OutChannel.BasicPublish(exchange: textBoxExchange.Text,
+                                     routingKey: textBoxRouteKey.Text,
+                                     basicProperties: null,
+                                     body: body);
+
+            _Log("Send body");
+        }
+
+        //
+        private void buttonStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        //
+        private bool _MQInit()
+        {
+            try
+            {
+                m_Factory = new ConnectionFactory()
+                {
+                    HostName = textBoxServer.Text,
+                    Port = 5672,
+                    UserName = textBoxAccount.Text,
+                    Password = textBoxPassword.Text,
+                    VirtualHost = "/",
+                };
+
+                m_Connection = m_Factory.CreateConnection();
+
+                m_OutChannel = m_Connection.CreateModel();
+                m_OutChannel.ExchangeDeclare(textBoxExchange.Text, ExchangeType.Direct);
+                m_OutChannel.QueueDeclare($"{textBoxOutQuene.Text}", true, false, false, null);
+                m_OutChannel.QueueBind($"{textBoxOutQuene.Text}", textBoxExchange.Text, textBoxRouteKey.Text, null);
+
+                //
+                m_InChannel = m_Connection.CreateModel();
+                m_InChannel.QueueDeclare(textBoxInQuene.Text, true, false, false, null);
+
+                m_InConsumer = new EventingBasicConsumer(m_InChannel);
+                m_InChannel.BasicConsume(textBoxInQuene.Text, true, m_InConsumer);
+                m_InConsumer.Received += _InConsumer_Received;
+            }
+            catch (Exception ex)
+            {
+                _Log(ex.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void _InConsumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            try
+            {
+                var body = e.Body;
+                var message = Encoding.UTF8.GetString(body.ToArray());
+
+                _Log("Received : " + message);
+            }
+            catch (Exception ex)
+            {
+                _Log(ex.Message);
+            }
+        }
+
+        //
+        private void _MQDestroy()
+        {
+            if (m_InChannel != null)
+            {
+                m_InChannel.Close();
+                m_InChannel.Dispose();
+                m_InChannel = null;
+            }
+            
+            if (m_OutChannel != null)
+            {
+                m_OutChannel.Close();
+                m_OutChannel.Dispose();
+                m_OutChannel = null;
+            }
+
+            if(m_Connection != null)
+            {
+                m_Connection.Close();
+                m_Connection.Dispose();
+                m_Connection = null;
+            }
+
+            if(m_Factory != null)
+            {
+                m_Factory = null;
+            }
+        }
+
+        //
+        private void _Log(string strText)
+        {
+            listBoxLog.Invoke(new EventHandler((obj, evt) =>
+            {
+                listBoxLog.Items.Add(strText);
+            }));
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            m_bIsMQConnect = _MQInit();
+            _Log($"MQ Connect {(m_bIsMQConnect == true ? "Ok" : "Failed")}");
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            _MQDestroy();
+            _Log("MQ  Destroy");
+            m_bIsMQConnect = false;
+        }
+
+        //
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            _SendBody();
+        }
+    }
+}
